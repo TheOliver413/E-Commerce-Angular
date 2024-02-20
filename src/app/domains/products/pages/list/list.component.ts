fcba0180908e755695744202a612a1abed497df3
OliverBorda <oliver.borda.avam@posbrava.io>
2024-02-20 10:19:04 -0500

    Creacion de header y ajustes de estilos

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { ProductComponent } from './../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -12,24 +13,48 @@ export class ListComponent {
 
   products = signal<Product[]>([]);
 
-  constructor () {
+  constructor() {
     const initProducts: Product[] = [
-      { id: Date.now(),
+      {
+        id: Date.now(),
         title: 'Product 1',
         price: 100,
         image: 'https://picsum.photos/640/640?r=23',
         createAt: new Date().toDateString(),
       },
-      { id: Date.now(),
+      {
+        id: Date.now(),
         title: 'Product 2',
         price: 100,
         image: 'https://picsum.photos/640/640?r=24',
         createAt: new Date().toDateString(),
       },
-      { id: Date.now(),
+      {
+        id: Date.now(),
         title: 'Product 3',
         price: 100,
         image: 'https://picsum.photos/640/640?r=25',
+        createAt: new Date().toDateString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Product 4',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=26',
+        createAt: new Date().toDateString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Product 5',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=27',
+        createAt: new Date().toDateString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Product 6',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=28',
         createAt: new Date().toDateString(),
       },
     ];
