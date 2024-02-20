8a728144cb8731dfb7f93c54a6f65c0151bcb65c
OliverBorda <oliver.borda.avam@posbrava.io>
2024-02-20 10:01:17 -0500

    Creacion componente products

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from './../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
 @Component({
   selector: 'app-list',
   standalone: true,
@@ -8,6 +9,33 @@ import { ProductComponent } from './../../components/product/product.component';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor () {
+    const initProducts: Product[] = [
+      { id: Date.now(),
+        title: 'Product 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23',
+        createAt: new Date().toDateString(),
+      },
+      { id: Date.now(),
+        title: 'Product 2',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=24',
+        createAt: new Date().toDateString(),
+      },
+      { id: Date.now(),
+        title: 'Product 3',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=25',
+        createAt: new Date().toDateString(),
+      },
+    ];
+    this.products.set(initProducts);
+  }
+
   fromChild(event: string) {
     console.log('Estamos en el padre');
     console.log(event);
