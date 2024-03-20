37db7b18897521462f029d6ddd1274161d45419a
OliverBorda <oliver.borda.avam@posbrava.io>
2024-03-20 10:06:18 -0500

    carrito de compra

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,16 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
   hideSideMenu = signal(true);
+  @Input({required: true}) cart: Product[] = [];
+  total = signal(0);
 
   toogleSideMenu() {
     this.hideSideMenu.update(prevState => !prevState);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const cart = changes['cart'];
+    if (cart) {
+      this.total.set(this.calcTotal());
+    }
+  }
+
+  calcTotal() {
+    return this.cart.reduce((total, product) => total + product.price, 0);
   }
 }
